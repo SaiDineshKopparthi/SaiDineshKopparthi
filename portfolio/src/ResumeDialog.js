0fa967bfaf89f2c1145f72cf6928d3f953ea39a8
Sai Dinesh Kopparthi <kopparthi.dinesh42224@gmail.com>
2024-08-11 19:49:03 -0500

    Resume changed from docx to pdf

--- portfolio/src/ResumeDialog.js
+++ portfolio/src/ResumeDialog.js
@@ -3,29 +3,35 @@ import './ResumeDialog.css';
 
 const ResumeDialog = ({ isOpen, onClose }) => {
     return (
+        <>
+        <div className={`overlay ${isOpen ? 'visible' : ''}`}></div>
+        
         <dialog className="nes-dialog" open={isOpen}>
             <form method="dialog">
                 <p>Resume</p>
                 <p>Select an option:</p>
                 <menu className="dialog-menu">
                     <a
-                        href="/RESUME.docx"
+                        href="/RESUME.pdf"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="nes-btn is-primary btn-with-icon"
+                        onClick={onClose}
+                    >
+                        View Online
+                    </a>
+                    <a
+                        href="/RESUME.pdf"
                         download
-                        className="nes-btn is-success"
+                        className="nes-btn is-success btn-with-icon"
                         onClick={onClose}
                     >
                         Download
                     </a>
-                    <button
-                        type="button"
-                        className="nes-btn"
-                        onClick={onClose}
-                    >
-                        Cancel
-                    </button>
                 </menu>
             </form>
         </dialog>
+        </>
     );
 }
 
