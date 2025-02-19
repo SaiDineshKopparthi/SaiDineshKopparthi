18fc86be75169d5ac7d2880a25b4dde97620475d
Sai Dinesh Kopparthi <kopparthi.dinesh42224@gmail.com>
2025-02-19 09:53:56 -0600

    Update About.js

--- portfolio-v0.2/src/components/About.js
+++ portfolio-v0.2/src/components/About.js
@@ -21,58 +21,20 @@ const About = () => {
 
         <div className="about-content nes-container with-title">
           <p className="title">Full Stack Developer</p>
-          <p>
-            I am a recent Master's graduate in Applied Computer Science from
-            Northwest Missouri State University, driven by an unending
-            enthusiasm for learning and a passion for advancing in the field of
-            computer science. With hands-on experience in developing iOS
-            applications and a solid background in web development using
-            technologies like React, Node.js, and Express, I thrive on the
-            challenge of creating innovative solutions. My roles have ranged
-            from enhancing user interfaces and integrating third-party
-            applications to supporting educational initiatives and contributing
-            to Agile transformations.
-          </p>
+          <p></p>
 
-          <p>
-            My career is marked by a commitment to staying at the forefront of
-            technology and achieving excellence in every project I undertake. I
-            am dedicated to exploring new technologies and leveraging my skills
-            to drive impactful results. My diverse experience, from developing
-            dynamic web components to optimizing geospatial data systems,
-            reflects my goal of pushing the boundaries of what's possible in
-            computer science.
-          </p>
+          <p></p>
         </div>
 
         <div className="about-content nes-container with-title">
           <p className="title">Developer</p>
-          <p>
-            I am a recent Master's graduate in Applied Computer Science from
-            Northwest Missouri State University, driven by an unending
-            enthusiasm for learning and a passion for advancing in the field of
-            computer science. With hands-on experience in developing iOS
-            applications and a solid background in web development using
-            technologies like React, Node.js, and Express, I thrive on the
-            challenge of creating innovative solutions. My roles have ranged
-            from enhancing user interfaces and integrating third-party
-            applications to supporting educational initiatives and contributing
-            to Agile transformations.
-          </p>
+          <p></p>
         </div>
 
         <div className="about-content nes-container with-title">
           <p className="title">Full Stack</p>
 
-          <p>
-            My career is marked by a commitment to staying at the forefront of
-            technology and achieving excellence in every project I undertake. I
-            am dedicated to exploring new technologies and leveraging my skills
-            to drive impactful results. My diverse experience, from developing
-            dynamic web components to optimizing geospatial data systems,
-            reflects my goal of pushing the boundaries of what's possible in
-            computer science.
-          </p>
+          <p></p>
         </div>
       </div>
     </section>
